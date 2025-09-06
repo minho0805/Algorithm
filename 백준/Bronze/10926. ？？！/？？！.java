a2a7b7ea224058fe0dde661f02f1ef176b423e12
김민호 <2022305011@skuniv.ac.kr>
2025-09-06 11:58:19 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String a = input.next();   // 문자열 입력 받기
+        System.out.println(a + "??!");
+
+        input.close();
+    }
+}
