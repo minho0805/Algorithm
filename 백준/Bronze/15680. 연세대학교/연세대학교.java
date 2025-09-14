78b4450fe666b97a03a74748105d70e6de761295
김민호 <2022305011@skuniv.ac.kr>
2025-09-14 20:56:39 +0900

    [Bronze V] Title: 연세대학교, Time: 180 ms, Memory: 17188 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if (n == 0){
+            System.out.println("YONSEI");
+        }
+        else{
+            System.out.println("Leading the Way to the Future");
+        }
+    }
+}
