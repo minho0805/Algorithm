e455bbbd1a9f06b7788e5d4d93750ded0c53cd42
김민호 <2022305011@skuniv.ac.kr>
2025-10-05 20:55:19 +0900

    [Bronze V] Title: 아스키 코드, Time: 184 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char alp = sc.nextLine().charAt(0);
+        int answer = (int) alp;
+        System.out.println(answer);
+    }
+}
